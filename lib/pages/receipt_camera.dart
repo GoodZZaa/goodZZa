ebccba9f721f2d4a18ad22fde948ef90f5bd52ab
hyejinxx <haejinjung1110@gmail.com>
2023-01-14 23:22:32 +1100

    feat: 카메라 촬영 추가

--- lib/pages/receipt_camera.dart
+++ lib/pages/receipt_camera.dart
@@ -1,7 +1,10 @@
 // A screen that takes in a list of cameras and the Directory to store images.
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class TakePictureScreen extends StatefulWidget {
   @override
@@ -10,7 +13,7 @@ class TakePictureScreen extends StatefulWidget {
 
 class TakePictureScreenState extends State<TakePictureScreen> {
   bool _cameraInitialized = false;
-  late CameraController _controller;
+  late CameraController? _controller;
   late CameraDescription _camera;
 
   @override
@@ -41,7 +44,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     _controller = CameraController(
         _camera, ResolutionPreset.max // 가장 높은 해상도의 기능을 쓸 수 있도록 합니다.
         );
-    _controller
+    _controller!
         .initialize()
         .then((value) => setState(() => _cameraInitialized = true));
   }
@@ -49,17 +52,60 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   @override
   void dispose() {
     // 위젯의 생명주기 종료시 컨트롤러 역시 해제시켜줍니다.
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return _cameraInitialized
-        ? CameraPreview(_controller)
+        ? Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: 1 / _controller!.value.aspectRatio,
+                child: CameraPreview(_controller!),
+              ),
+              Positioned(
+                  bottom: 10,
+                  right: 0,
+                  left: 0,
+                  child: InkWell(
+                    onTap: () async {
+                      print('촬영 클릭!');
+                      XFile? rawImage = await takePicture();
+                      if (rawImage != null) {
+                        File imageFile = File(rawImage.path);
+                        // 서버 전송
+                      }
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Icon(Icons.circle, color: Colors.white38, size: 70),
+                        Icon(Icons.circle, color: Colors.white, size: 55),
+                      ],
+                    ),
+                  ))
+            ],
+          )
         : const Center(
             child: CircularProgressIndicator(
             color: Color.fromARGB(0xFF, 0x54, 0xB1, 0x75),
           ));
+  }
+
+  Future<XFile?> takePicture() async {
+    final CameraController? cameraController = _controller;
+    if (cameraController!.value.isTakingPicture) {
+      return null;
+    }
+    try {
+      XFile file = await cameraController.takePicture();
+      print('카메라 촬영 성공');
+      return file;
+    } on CameraException catch (e) {
+      print('카메라 촬영 실패 $e');
+      return null;
+    }
   }
 }
